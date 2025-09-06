2a1284b7967ec28f1b95632352328a6dee273293
Osei Viacyn Kweku Ofori <addokweku524@gmai.com==-->
2025-09-06 23:54:22 +0000

    added userprofiel.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
+import UserProfile from "./components/UserProfile";
 
 function App() {
   return (
@@ -9,6 +10,10 @@ function App() {
         <Header />
         <MainContent />
         <Footer />
+      </div>
+
+      <div>
+        <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       </div>
     </>
   );
