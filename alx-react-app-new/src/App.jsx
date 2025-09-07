1052e0d0c849d2ddbe016aa00016ac61595ecd52
Osei Viacyn Kweku Ofori <addokweku524@gmai.com==-->
2025-09-07 00:20:14 +0000

    added css styling to jsx files

---
+++ alx-react-app-new/src/App.jsx
@@ -0,0 +1,37 @@
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
+import UserProfile from "./components/UserProfile";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Header />
+        <MainContent />
+        <Footer />
+      </div>
+
+       <div style={{ display: 'flex', justifyContent: 'center', flexWrap: 'wrap' }}>
+        <UserProfile 
+          name="Alice" 
+          age="25" 
+          bio="Loves hiking and photography" 
+        />
+        <UserProfile 
+          name="John" 
+          age="30" 
+          bio="Software engineer who enjoys traveling and cooking" 
+        />
+        <UserProfile 
+          name="Emma" 
+          age="28" 
+          bio="Passionate about art and design" 
+        />
+      </div>
+
+    </>
+  );
+}
+
+export default App;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,16 @@
+function MainContent() {
+  return (
+    <main
+      style={{
+        backgroundColor: "#8aa2b9ff",
+        padding: "20px",
+        textAlign: "center",
+        fontSize: "1.1rem",
+        marginTop: "10px",
+      }}
+    >
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+export default MainContent;
